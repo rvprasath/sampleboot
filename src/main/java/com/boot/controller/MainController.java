ad74d88fcacc412c9033d07b2814961d32381e0f
vishnu <vishnu@github.com>
2020-07-08 00:58:44 +0530

    modification

--- src/main/java/com/boot/controller/MainController.java
+++ src/main/java/com/boot/controller/MainController.java
@@ -1,8 +1,5 @@
 package com.boot.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +13,4 @@ public class MainController {
 		return "Hello World!";
 	}
 
-	@RequestMapping("/list")
-	public List<String> json() {
-		List<String> list = new ArrayList<String>();
-		list.add("Ram");
-		list.add("Viju");
-		list.add("Amaresh");
-		list.add("Damodaran");
-		list.add("Hari");
-		return list;
-	}
 }
